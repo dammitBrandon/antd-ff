b1dfb7ae1e48160f47697a4f3dda765e8bc05577
dammitBrandon <bailey1.brandon@gmail.com>
2020-08-31 16:07:45 -0700

    making some progress

--- src/AntForm.js
+++ src/AntForm.js
@@ -1,5 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form as AForm, Input } from 'antd';
+import { Button, Form, Form as AForm, Input } from 'antd';
+import { Field as FField, Form as FForm } from 'react-final-form';
+import styled from 'styled-components';
+import emailValidator from 'email-validator';
+import { FORM_ERROR } from 'final-form';
+
+export const required = value => {
+  return value || typeof value === 'number' ? undefined : 'Required';
+};
+
+export const email = value => {
+  return value && !emailValidator.validate(value) ? 'Invalid email address' : undefined;
+};
+
+export const edu = value => {
+  return value && !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.edu$/.test(value)
+    ? 'You must be actively enrolled in a College or University.'
+    : undefined;
+};
+
+export const yahoo = value => {
+  return value && /.+@yahoo\.com/.test(value)
+    ? 'Really? You still use Yahoo for your email?'
+    : undefined;
+};
+
+export const composeValidators = (...validators) => (value, allValues, fieldState) => {
+  return validators.reduce(
+    (error, validator) => error || validator(value, allValues, fieldState),
+    undefined
+  );
+};
+
+const CenteredContainer = styled.div`
+  height: 100%;
+  min-height: 100vh;
+  width: 60vw;
+  margin: 0 auto;
+  padding-top: 100px;
+  display: block;
+`;
 
 const formItemLayout = {
   labelCol: {
@@ -20,14 +60,60 @@ const formTailLayout = {
   },
 };
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const onSubmit = async values => {
+  await sleep(300)
+  // window.alert(JSON.stringify(values, 0, 2))
+  return { [FORM_ERROR]: 'Login Failed', email: 'Unknown email' }
+}
+
 export const AntForm = () => {
   const [form] = AForm.useForm();
-  console.info('form: ', form);
-  const [checkNick, setCheckNick] = useState(false);
+  console.info('AntForm, form: ', form);
 
   return (
-    <AForm form={form} name="ant-form">
-      Form
-    </AForm>
+    <CenteredContainer>
+      <FForm
+        name="ant-final-form"
+        onSubmit={onSubmit}
+        render={(props) => {
+          return (
+            <AForm onFinish={props.handleSubmit}>
+              {props.submitError && <div className="error">{props.submitError}</div>}
+              <FField name="email" type="text" placeholder="test@email.edu" label="email" validate={composeValidators(required, email)}>
+                {({input, placeholder, label, meta})=> (
+                  <>
+                    <Form.Item label={label}
+                               hasFeedback
+                               validateStatus={((meta.error && meta.submitFailed) || (meta.submitError && meta.submitFailed)) ? 'error' :
+                                 (meta.valid && meta.submitSucceeded) ? 'success' :
+                                   (meta.validating) ? 'warning' : ''
+                               }
+                               help={(meta.error || meta.submitError) && meta.touched && (
+                                 <span style={{color: '#ff4d4f'}}>{meta.error || meta.submitError}</span>
+                               )}
+                    >
+                      <Input {...input} placeholder={placeholder} label={label} />
+                    </Form.Item>
+                    <pre>{JSON.stringify(meta, 0, 2)}</pre>
+                  </>
+                )}
+              </FField>
+              
+              <Button
+                type="primary"
+                htmlType="submit"
+              >
+                Submit
+              </Button>
+              <pre>{JSON.stringify(props.submitError, 0, 2)}</pre>
+              errors: <pre>{JSON.stringify(props.errors, 0, 2)}</pre>
+              error: <pre>{JSON.stringify(props.error, 0, 2)}</pre>
+            </AForm>
+          )
+        }}
+      />
+    </CenteredContainer>
   );
 }
--- src/FinalForm.js
+++ src/FinalForm.js
@@ -8,6 +8,15 @@ import setFieldData from 'final-form-set-field-data'
 import { FORM_ERROR } from 'final-form'
 import styled from 'styled-components';
 
+const CenteredContainer = styled.div`
+  height: 100%;
+  min-height: 100vh;
+  width: 60vw;
+  margin: 0 auto;
+  padding-top: 100px;
+  display: block;
+`;
+
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const onSubmit = async (values, form) => {
@@ -49,71 +58,75 @@ const validateMessages = {
   },
 };
 
-export const FinalForm = () => (
-  <FForm
-    {...formItemLayout}
-    debug={(state, fieldStates) => {
-      console.log('debugging, state: ', state);
-      console.log('debugging, fieldStates: ', fieldStates);
-      
-    }}
-    onSubmit={onSubmit}
-    mutators={{
-      setFieldData
-    }}
-    render={({submitError, handleSubmit, mutators, submitting, pristine, values}) => {
-      return (
-        <Form
-          onFinish={handleSubmit}
-          // validateMessages={validateMessages}
-          onFinishFailed={(err) => {
-            console.error('Form failed, err: ', err);
-          }}
-        >
-          {submitError && <div className="error">{submitError}</div>}
-          <FField name="username" type="text" placeholder="username" label="username" >
-            {({input, placeholder, label, meta})=> (
-              <>
-                <Form.Item label={label}
-                           hasFeedback
-                           validateStatus={(meta.submitError && meta.submitFailed) ? 'error' :
-                             (meta.valid && meta.submitSucceeded) ? 'success' :
-                               (meta.validating) ? 'warning' : ''
-                           }
-                >
-                  <Input {...input} placeholder={placeholder} label={label} />
-                </Form.Item>
-                <pre>{JSON.stringify(meta, 0, 2)}</pre>
-              </>
-            )}
-          </FField>
-          
-          <FField name="password" type="text" placeholder="password" label="password">
-            {({input, placeholder, label, meta})=> (
-              <>
-                <Form.Item label={label}
-                           hasFeedback
-                           validateStatus={(meta.submitError && meta.submitFailed) ? 'error' :
-                             (meta.valid && meta.submitSucceeded) ? 'success' :
-                               (meta.validating) ? 'warning' : ''
-                           }
-                >
-                  <Input.Password {...input} placeholder={placeholder} label={label} />
-                </Form.Item>
-                <pre>{JSON.stringify(meta, 0, 2)}</pre>
-              </>
-            )}
-          </FField>
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
+export const FinalForm = () => {
+  return (
+    <CenteredContainer>
+      <FForm
+        {...formItemLayout}
+        debug={(state, fieldStates) => {
+          console.log('debugging, state: ', state);
+          console.log('debugging, fieldStates: ', fieldStates);
+        
+        }}
+        onSubmit={onSubmit}
+        mutators={{
+          setFieldData
+        }}
+        render={({submitError, handleSubmit, mutators, submitting, pristine, values}) => {
+          return (
+            <Form
+              onFinish={handleSubmit}
+              // validateMessages={validateMessages}
+              onFinishFailed={(err) => {
+                console.error('Form failed, err: ', err);
+              }}
             >
-              Submit
-            </Button>
-          </Form.Item>
-          <pre>{JSON.stringify(values, 0, 2)}</pre>
-        </Form>
-      )}}
-  />
-)
+              {submitError && <div className="error">{submitError}</div>}
+              <FField name="username" type="text" placeholder="username" label="username" >
+                {({input, placeholder, label, meta})=> (
+                  <>
+                    <Form.Item label={label}
+                               hasFeedback
+                               validateStatus={(meta.submitError && meta.submitFailed) ? 'error' :
+                                 (meta.valid && meta.submitSucceeded) ? 'success' :
+                                   (meta.validating) ? 'warning' : ''
+                               }
+                    >
+                      <Input {...input} placeholder={placeholder} label={label} />
+                    </Form.Item>
+                    <pre>{JSON.stringify(meta, 0, 2)}</pre>
+                  </>
+                )}
+              </FField>
+            
+              <FField name="password" type="text" placeholder="password" label="password">
+                {({input, placeholder, label, meta})=> (
+                  <>
+                    <Form.Item label={label}
+                               hasFeedback
+                               validateStatus={(meta.submitError && meta.submitFailed) ? 'error' :
+                                 (meta.valid && meta.submitSucceeded) ? 'success' :
+                                   (meta.validating) ? 'warning' : ''
+                               }
+                    >
+                      <Input.Password {...input} placeholder={placeholder} label={label} />
+                    </Form.Item>
+                    <pre>{JSON.stringify(meta, 0, 2)}</pre>
+                  </>
+                )}
+              </FField>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                >
+                  Submit
+                </Button>
+              </Form.Item>
+              <pre>{JSON.stringify(values, 0, 2)}</pre>
+            </Form>
+          )}}
+      />
+    </CenteredContainer>
+  )
+}
